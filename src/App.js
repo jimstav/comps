7fedfe4b9e01e1d61ca162e8b93c2a1afc196a2a
Dimitris Stavroulakis <jimstav@gmail.com>
2025-01-22 23:31:12 +0200

    Reminder on Building Lists

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Accordion from "./components/Accordion";
 function App() {
   const items = [
     {
+      id: "asadas",
       label: "Can I use React on a project?",
       content: "You can use React on any project you want.",
     },
     {
+      id: "dagdgfh",
       label: "Can I use JavaScript on a project?",
       content: "You can use JavaScript on any project you want.",
     },
     {
+      id: "fgdgfd",
       label: "Can I use CSS on a project?",
       content: "You can use CSS on any project you want.",
     },
