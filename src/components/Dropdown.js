695d5cc2794730cbab050f5e0280ca39c6453bc8
Dimitris Stavroulakis <jimstav@gmail.com>
2025-01-26 17:01:00 +0200

    Reminder on Event Handlers in Maps

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -7,8 +7,17 @@ function Dropdown({ options }) {
     setIsOpen((currentIsOpen) => !currentIsOpen);
   };
 
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    console.log(option);
+  };
+
   const renderedOptions = options.map((option) => {
-    return <div key={option.value}>{option.label}</div>;
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
   });
 
   return (
