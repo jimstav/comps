bfa9f4ef41a97e53b280c9490a09034becd7454e
Dimitris Stavroulakis <jimstav@gmail.com>
2025-01-26 14:38:53 +0200

    Component Creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const items = [
-    {
-      id: "asadas",
-      label: "Can I use React on a project?",
-      content: "You can use React on any project you want.",
-    },
-    {
-      id: "dagdgfh",
-      label: "Can I use JavaScript on a project?",
-      content: "You can use JavaScript on any project you want.",
-    },
-    {
-      id: "fgdgfd",
-      label: "Can I use CSS on a project?",
-      content: "You can use CSS on any project you want.",
-    },
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
   ];
-  return <Accordion items={items} />;
+  return <Dropdown options={options} />;
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,24 @@
+import Accordion from "./components/Accordion";
+
+function AccordionPage() {
+  const items = [
+    {
+      id: "asadas",
+      label: "Can I use React on a project?",
+      content: "You can use React on any project you want.",
+    },
+    {
+      id: "dagdgfh",
+      label: "Can I use JavaScript on a project?",
+      content: "You can use JavaScript on any project you want.",
+    },
+    {
+      id: "fgdgfd",
+      label: "Can I use CSS on a project?",
+      content: "You can use CSS on any project you want.",
+    },
+  ];
+  return <Accordion items={items} />;
+}
+
+export default AccordionPage;
