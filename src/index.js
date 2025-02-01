a902984fca1266855be5b07fd2e162119b0ff923
Dimitris Stavroulakis <jimstav@gmail.com>
2025-02-01 14:39:00 +0200

    Navigation Context

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
+import { NavigationProvider } from "./context/navigation.js";
 
 const el = document.getElementById("root");
 const root = ReactDom.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
